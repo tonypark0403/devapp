54909b7bb2fe3d765244f388fb3afb486833940a
Tony Park <tonypark0403@gmail.com>
2019-02-18 14:06:13 -0500

    added mongodb connection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 import homeRouter from './routes/homeRouter';
 import routes from "./routes";
+import dbKey from './config/keys';
 
 const app = express();
+
+//DB Config
+const db = dbKey.mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 //Middlewares
 app.use(morgan("dev"));
---
+++ src/config/keys.js
@@ -0,0 +1,3 @@
+export default {
+    mongoURI: 'mongodb://devapp:devapp123@ds151943.mlab.com:51943/devapp'
+}
